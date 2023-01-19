e656980ccadd2b9bd0a1ee3cccda92cc0d8a9792
yongari <ykjeon4061@hotmail.com>
2023-01-19 15:03:15 +0900

    new file

---
+++ 1.py
@@ -0,0 +1,69 @@
+
+#
+#print("1+4=5")
+#print()
+#print("1+4=5")
+
+
+#string_a = "{}{}".format(10,20);
+#print(string_a);
+'''
+int_a = 101;
+
+
+string_a = str(int_a);
+print(string_a);
+'''
+
+# a = "12,3,4,5".split(",");
+# print(a)
+'''
+int_a = '101은 {}'.format(22)
+print(int_a)
+int_a = f'101은 {22}'
+print(int_a)
+'''
+'''
+if True:
+    print(True)
+if False:
+    print(False)
+
+print(1,2,3,4,5)
+'''
+'''
+import datetime
+now = datetime.datetime.now();
+print(now.year)
+'''
+'''
+str_a = "         Hello World   "
+str_b = str_a.strip()
+print(str_a)
+print(str_b)
+'''
+'''
+i = 4
+if i == 1 \
+    or i == 2:
+    print(1,2)
+
+if i == 1 or i == 2:
+    pass
+elif i == 3 or i == 4:
+    print(111,222)
+
+raise NotImplementedError
+'''
+
+a = [0,1,2,3]
+print(a[1:3])
+print(a)
+print(len(a))
+
+
+
+
+
+
+
